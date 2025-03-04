150f7b1699a384446451b09bd4837d1fbee0a183
Mateusz Jaworski <jaworskimat.priv@gmail.com>
2025-03-04 18:17:32 +0100

    solution

---
+++ 04_03_25/main.py
@@ -0,0 +1,27 @@
+import cv2
+
+def main():
+    image = cv2.imread("cat.png")
+
+    if image is None:
+        print("File not found!")
+        return
+    
+    image_gray = cv2.imread("cat.png", cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite("cat_sad.png", image_gray)
+
+    color_img_channels = image.shape[-1]
+    gray_img_channels = 1
+
+    print(f'Color image channels: {color_img_channels}')
+    print(f'Color image channels: {gray_img_channels}')
+
+    cv2.namedWindow("Display", cv2.WINDOW_AUTOSIZE) 
+    cv2.imshow("color image ", image)
+    cv2.imshow("gray image", image_gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 04_03_25/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+    cv2.waitKey(0) # Czeka na dowolny klawisz, by zamknąć okno
+    cv2.destroyAllWindows() # Zamknięcie wszystkich okien
